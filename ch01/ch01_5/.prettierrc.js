2b7903ad75c15d5255af8ac4e31e0843d64e1ffa
rudaks <rudaks94@gmail.com>
2024-03-18 19:05:23 +0900

    update

---
+++ ch01/ch01_5/.prettierrc.js
@@ -0,0 +1,10 @@
+// .prettierrc.js 내용을 만들어줘
+module.exports = {
+  bracketSpacing: false,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  printWidth: 90,
+  tabWidth: 2
+}
---
+++ ch02/ch02_1/src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+const App = () => {
+  return (
+      <div>Hello</div>
+  )
+}
+
+export default App;
